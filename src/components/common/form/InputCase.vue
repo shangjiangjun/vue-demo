9f420d705af932a26d7d7e3d0557226a172fa495
xuyh <574098101@qq.com>
2022-08-29 20:33:40 +0800

    Update 自定义组件：监听子组件事件，不同方法

---
+++ src/components/common/form/InputCase.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="input-case">
+    <input
+      v-bind:value="value"
+      v-on:input="$emit('change', $event.target.value)" >
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "InputCase",
+    props: ['value'],
+    data () {
+      return {}
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+</style>
--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="index">
     <h1>{{ msg }}</h1>
-    <div class="menu">
+    <div>
       <h4>webSocket</h4>
       <button v-on:click="sendWs">发送信息</button>
+    </div>
+    <div>
+      <h4>组件使用v-model</h4>
+      <label>改变input的值触发事件</label>
+      <input-case
+        v-bind:value="inputValue"
+        v-on:change="handleChangeValue"
+        ></input-case>
     </div>
   </div>
 </template>
 
 <script>
+  // 1. 引入自定义组件
+  import InputCase from '@/components/common/form/InputCase.vue'
+
   export default {
     name: "Index",
+    // 2. 引入数据
+    components: {
+      InputCase
+    },
     data () {
       return {
-        msg: 'Welcome to Your Vue'
+        msg: 'Welcome to Your Vue',
+
+        // 自定义组件
+        inputValue: "hello"
       }
     },
     methods: {
@@ -24,6 +42,10 @@
             console.log('接收信息', msgEvent)
           }
         }
+      },
+      // 绑定组件修改数据
+      handleChangeValue(e){
+        console.log(e)
       }
     }
   }
